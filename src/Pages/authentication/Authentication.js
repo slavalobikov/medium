e4916cea4e130dd4afb6d4c21136894ff2eef0bb
lobik <lobikova12@mail.ru>
2020-07-09 15:32:37 +0300

    autorization and registration with API

--- src/Pages/authentication/Authentication.js
+++ src/Pages/authentication/Authentication.js
@@ -1,34 +1,49 @@
 import React, {useEffect, useState} from 'react';
-import {NavLink} from "react-router-dom";
-import axios from 'axios'
+import {NavLink, Redirect} from "react-router-dom";
 
 import s from './Authentication.module.css'
 import useFetch from "../../Hooks/useFetch";
 
-const Authentication = () => {
+const Authentication = (props) => {
+
+    const isLogin = props.match.path === '/login';
+
+    const pageTitle = isLogin ? 'Авторизация' : 'Регистрация';
+    const descriptionLink = isLogin
+        ? <NavLink to={'/register'}>Нету аккаунта? Зарегистрируйтесь!</NavLink>
+        : <NavLink to={'/login'}>У вас уже есть аккаунт?</NavLink>;
+    const apiURL = isLogin ? 'users/login': 'users';
+    const buttonText = isLogin ? 'Войти' : 'Зарегистрироваться';
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [username, setUsername] = useState('');
+    const [isSuccessfulSubmit, setIsSuccessfulSubmit] = useState(false);
+    //const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const [{response, isLoading, error}, doFetch] = useFetch('users/login')
-    console.log('dfdff',response,isLoading,error)
+    const [{response, isLoading, error}, doFetch] = useFetch(apiURL);
+/*
+    console.log('dfdff',response,isLoading,error);
+*/
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('email', email );
-    console.log('password', password );
-    //setIsSubmitting(true);
+    const user = isLogin ? {email, password} : {email, password, username}
     doFetch({
         method: 'post',
         data: {
-            user: {
-                email: 'qqq@qq.com',
-                password: '123'
-            }
+            user
         }
     })
 };
+
+useEffect(() => {
+    if (!response ) {
+        return
+    }
+    localStorage.setItem('token', response.user.token);
+    setIsSuccessfulSubmit(true);
+}, [response]);
     /*useEffect(() => {
         if (!isSubmitting) {
           return
@@ -50,19 +65,23 @@ const handleSubmit = (e) => {
 
     })
     });*/
-
-
+    if (isSuccessfulSubmit) {
+        return <Redirect to={'/'}/>
+    }
     return (
         <div className={s.content}>
-            <div><h1 className={s.h}>Авторизация</h1></div>
-            <div className={s.reg}><NavLink to={'/register'}>Нету аккаунта? Зарегистрируйтесь!</NavLink></div>
+            <div><h1 className={s.h}>{pageTitle}</h1></div>
+            <div className={s.reg}>{descriptionLink}</div>
             <form>
+                {!isLogin &&
+                    <div className={s.reg}><input className={s.inp} type="text" placeholder={'Имя пользователя'}
+                        value={username} onChange={e => setUsername(e.currentTarget.value)}/></div>
+                }
                 <div className={s.reg}><input type={'email'} className={s.inp} placeholder={'Почта'}
                     value={email} onChange={e => setEmail(e.target.value)}/></div>
                 <div className={s.reg}><input type={'password'} className={s.inp} placeholder={'Пароль'}
                     value={password} onChange={e => setPassword(e.target.value)}/></div>
-                <div className={s.reg}><button onClick={handleSubmit} disabled={isSubmitting}>Войти</button></div>
-
+                <div className={s.reg}><button onClick={handleSubmit} disabled={isLoading}>{buttonText}</button></div>
             </form>
         </div>
     )
