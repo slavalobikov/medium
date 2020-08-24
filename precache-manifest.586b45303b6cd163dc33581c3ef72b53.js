e2afeef72cbf826194b3f097f0cde75d1cb33835
lobik <lobikova12@mail.ru>
2020-08-24 23:12:58 +0300

    Updates

---
+++ precache-manifest.586b45303b6cd163dc33581c3ef72b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f605950a1c81bbc411a672c61204e96",
+    "url": "/medium/index.html"
+  },
+  {
+    "revision": "3934501b1c53f5fcd969",
+    "url": "/medium/static/css/main.9ae3c600.chunk.css"
+  },
+  {
+    "revision": "78aa239ffed7fafc05f6",
+    "url": "/medium/static/js/2.ea08ef2d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/medium/static/js/2.ea08ef2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3934501b1c53f5fcd969",
+    "url": "/medium/static/js/main.26273b5d.chunk.js"
+  },
+  {
+    "revision": "ff1a1b9d0baaa5ac1427",
+    "url": "/medium/static/js/runtime-main.e5fcb774.js"
+  }
+]);
